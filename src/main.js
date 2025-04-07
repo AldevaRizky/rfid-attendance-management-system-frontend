1bc12ae53e15a28b4a90fec4d2ad1aa5ce1a6dad
Aldeva Rizky <aldevarwpp@gmail.com>
2025-04-07 19:37:28 +0700

    feat(ui): replace Pinia with Vuetify and apply custom light theme

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+          success: '#4CAF50',
+          error: '#FF5252',
+          warning: '#FB8C00'
+        }
+      }
+    }
+  }
+})
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(vuetify)
+  .use(router)
+  .mount('#app')
